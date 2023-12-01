249d8307f3be08714cccefa1d3296a63eb86718a
Vaibhav Gupta <vaibhav0000.vg@gmail.com>
2023-12-01 18:17:01 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements] 
+
+setup(
+    name='ml_project',
+    version='0.0.1',
+    author='Vaibhav',
+    author_email='vaibhav0000.vg@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
